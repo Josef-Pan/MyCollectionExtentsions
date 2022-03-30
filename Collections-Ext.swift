56daf00225074996f34a379a0ca296af84b61783
Josef <panjunfeng@hotmail.com>
2022-03-30 18:08:03 +1000

    Update Collections-Ext.swift

--- Collections-Ext.swift
+++ Collections-Ext.swift
@@ -5,4 +5,14 @@ extension Collection {
     subscript(safeIdx index: Index) -> Element? {
         get { return self.indices.contains(index) ? self[index] : nil }
     }
+    
+    /// Do operations cocurrently for each element
+    /// [ 1, 3, 5, 7, 9].forEachInParallel { item in
+    ///     print(item)
+    /// }
+    func forEachInParallel(_ each: (Self.Element) -> Void) {
+        DispatchQueue.concurrentPerform(iterations: count) {
+            each(self[index(startIndex, offsetBy: $0)])
+        }
+    }
 }
